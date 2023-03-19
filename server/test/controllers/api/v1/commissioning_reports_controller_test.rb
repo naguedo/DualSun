9f915a7a5029012cd5f8e7e5bd2fa6d28e19a5e0
Damien Brincourt <damien.brincourt@qoqa.com>
2023-03-19 13:21:15 +0100

    fix and improve test controller

--- server/test/controllers/api/v1/commissioning_reports_controller_test.rb
+++ server/test/controllers/api/v1/commissioning_reports_controller_test.rb
@@ -3,18 +3,21 @@ require 'test_helper'
 module Api::V1
   class CommissioningReportsControllerTest < ActionDispatch::IntegrationTest
     test "should create commissioning report" do
-      post api_v1_commissioning_reports_url, params: { commissioning_report: {
-        company_name: "SoleilVert",
-        siren: "123456789",
-        phone: "0123456789",
-        email: "jeandupont@apptest.com",
-        name: "Jean Dupont",
-        street: "9 Rue de la République",
-        city: "Bordeaux",
-        zip: 33000,
-        installed_on: Date.today,
-        option: :photovoltaic
-      }}, as: :json
+      assert_difference('CommissioningReport.count') do
+        post api_v1_commissioning_reports_url, params: { commissioning_report: {
+          company_name: "SoleilVert",
+          siren: "123456789",
+          phone: "0123456789",
+          email: "jeandupont@apptest.com",
+          name: "Jean Dupont",
+          street: "9 Rue de la République",
+          city: "Bordeaux",
+          zip: 33000,
+          installed_on: Date.today,
+          option: :photovoltaic,
+          panels_attributes: [{ identifier: 123456 }]
+        }}, as: :json
+      end
 
       assert_response :success
     end
@@ -30,7 +33,8 @@ module Api::V1
         city: "",
         zip: "",
         installed_on: "",
-        option: ""
+        option: "",
+        panels: nil
       }}, as: :json
 
       assert_response :unprocessable_entity
